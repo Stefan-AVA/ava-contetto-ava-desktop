b86ddccdf63a9ccba96f36fd8d945f4d0d5054c6
pyon123 <pyon123@outlook.com>
2024-02-14 09:59:40 -0800

    update forge config

--- forge.config.js
+++ forge.config.js
@@ -1,12 +1,32 @@
+const { version } = require('./package.json')
+
 module.exports = {
   packagerConfig: {
+    appId: "com.avahome.ava.desktop",
+    name: "Contetto",
+    icon: "build/icon",
     asar: true,
+    osxSign: {
+      identity: "Developer ID Application: *",
+      hardenedRuntime: true,
+      'gatekeeper-assess': false
+    },
+    // osxNotarize: {
+    //   tool: "notarytool",
+    //   appleId: process.env.APPLE_ID,
+    //   appleIdPassword: process.env.APPLE_APP_PASSWORD
+    // }
   },
   rebuildConfig: {},
   makers: [
     {
       name: "@electron-forge/maker-squirrel",
-      config: {},
+      config: {
+        name: "Contetto",
+        setupIcon: "build/icon.ico",
+        setupExe: `contettoSetup.exe`,
+        noMsi: true,
+      },
     },
     {
       name: "@electron-forge/maker-zip",
